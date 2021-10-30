134a7a127ba4fa8ab26910a4bc86c04086db2d64
killcrafter1 <kilian-hauber@gmx.de>
2021-10-31 00:55:17 +0200

    first release

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkblue: '#15192A',
+        mainblue: '#1F263C',
+        lightgray: '#737B92',
+        grayblue: '#1A1F32',
+        herugreen: '#00D084',
+        herured: '#ED4245',
+        discordblue:'#5865F2',
+        ultrablue:'#0076FF'
+      },
+      fontFamily: {
+        nunito: ["'Nunito', sans-serif"],
+        delagothicone: ["'Dela Gothic One', cursive"],
+        poppins: ["'Poppins', sans-serif"]
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+
+  }
+}
